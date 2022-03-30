9b60da0bb5d57cd2c0dca5c23087fa34d79cc155
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-30 12:51:28 +0400

    task 1

---
+++ collection/collection/BasketList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace collection
+{
+    class BasketList<T,U>
+    {
+        public U[] Vitamins;
+
+        public T[] Fruits;
+
+        
+        public BasketList()
+        {
+            Fruits = new T[0];
+            Vitamins = new U[0];
+
+        }
+
+        
+
+
+        public void Add(T fruit,U vitamin)
+        {
+            Array.Resize(ref Fruits, Fruits.Length + 1);
+            Fruits[Fruits.Length - 1] = fruit;
+            Array.Resize(ref Vitamins, Vitamins.Length + 1);
+            Vitamins[Vitamins.Length - 1] = vitamin;
+        }
+
+        public T[] getFruits()
+        {
+            return Fruits;
+        }
+
+        public U[] getVitamins()
+        {
+            return Vitamins;
+        }
+
+    }
+}
---
+++ collection/collection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasketList<string, char> basketList = new BasketList<string, char>();
+
+            basketList.Add("LEMON", 'C');
+            basketList.Add("PINEAPLE", 'A');
+            basketList.Add("APPLE", 'D');
+
+            Console.WriteLine("Meyveler");
+            
+            foreach (string item in basketList.Fruits)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Vitaminleri");
+            
+
+            foreach (char item in basketList.Vitamins)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
